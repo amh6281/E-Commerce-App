465ca24f8a28b68f6d92fa7d0bd9b9e564196312
amh6281 <amh6281@naver.com>
2022-02-17 16:48:50 +0900

    Navbar 생성 2022.02.17

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Event from "../components/Event";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Event />
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
