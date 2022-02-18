3a99eb2c82f26435a9c3ed443c74d5753ff84311
amh6281 <amh6281@naver.com>
2022-02-18 15:29:48 +0900

    Slider 생성 2022.02.18
    Slider 컴포넌트를 생성하고, 버튼 클릭 시 useState()를 통해 다음 상품을 보여준다.

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://o.remove.bg/downloads/e276f9a7-c404-4407-8e07-cdcfb46c3994/roman-shilin-12ybyCCwXYU-unsplash-removebg-preview.png",
+    title: "신상품 특별전",
+    desc: "신상품 특별전입니다. 22SS 5차 신상품들을 30% 할인과 함께 만나보세요.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://o.remove.bg/downloads/47d15918-f0bd-4ae1-ac41-b67fcf25156d/taras-chernus--au3XMzfhro-unsplash-removebg-preview.png",
+    title: "22 S/S 봄 컬렉션 한정 발매",
+    desc: "Basic & Point가 돋보이는 22 S/S 봄 1차 컬렉션의 한정 발매 기획전입니다. 신상품을 단독 최대 56%의 가격으로 만나보세요.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://o.remove.bg/downloads/2a77871c-c241-4fdc-a497-3f9756fe594c/photo-1622519407650-3df9883f76a5-removebg-preview.png",
+    title: "신학기 아이템 모음전",
+    desc: "트렌디하면서도 무드를 잃지 않은 디자인으로 만족할 수 있는 22 S/S 1차 컬렉션을 먼저 만나보세요.",
+    bg: "fbf0f4",
+  },
+];
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Event from "../components/Event";
 import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
       <Event />
       <Navbar />
+      <Slider />
     </div>
   );
 };
