1b3849bf0ac2a388c46fad579cb322def3b8a9df
amh6281 <amh6281@naver.com>
2022-02-21 16:08:58 +0900

    ProductList 페이지 추가 2022.02.21
    전체 상품과 상품의 사이즈, 색상, 정렬방식 등의 옵션을 추가한다.

---
+++ src/pages/ProductList.js
@@ -0,0 +1,83 @@
+import React from "react";
+import styled from "styled-components";
+import Event from "../components/Event";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+
+const Option = styled.option``;
+
+const ProductList = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Event />
+      <Title>상품</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Detail</FilterText>
+          <Select>
+            <Option disabled selected>
+              Color
+            </Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Gray</Option>
+            <Option>Beige</Option>
+            <Option>Yellow</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Size
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>정렬방식</FilterText>
+          <Select>
+            <Option selected>신상품</Option>
+            <Option>낮은 가격</Option>
+            <Option>높은 가격</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default ProductList;
