1d7d5ce2acf62c1888917e2fe4584594f813675e
amh6281 <amh6281@naver.com>
2022-02-21 14:29:13 +0900

    Product 컴포넌트 추가 2022.02.21
    data.js에 Products를 추가하고, Products 컴포넌트에서 data.js의 Products를 Product 컴포넌트에게 전달한다.

--- src/data.js
+++ src/data.js
@@ -1,21 +1,33 @@
+import sliderImg1 from "./image/slider/sliderImg1.png";
+import sliderImg2 from "./image/slider/sliderImg2.png";
+import sliderImg3 from "./image/slider/sliderImg3.png";
+import productImg1 from "./image/product/productImg1.png";
+import productImg2 from "./image/product/productImg2.png";
+import productImg3 from "./image/product/productImg3.png";
+import productImg4 from "./image/product/productImg4.png";
+import productImg5 from "./image/product/productImg5.png";
+import productImg6 from "./image/product/productImg6.png";
+import productImg7 from "./image/product/productImg7.png";
+import productImg8 from "./image/product/productImg8.png";
+
 export const sliderItems = [
   {
     id: 1,
-    img: "https://o.remove.bg/downloads/e276f9a7-c404-4407-8e07-cdcfb46c3994/roman-shilin-12ybyCCwXYU-unsplash-removebg-preview.png",
+    img: sliderImg1,
     title: "신상품 특별전",
     desc: "신상품 특별전입니다. 22SS 5차 신상품들을 30% 할인과 함께 만나보세요.",
     bg: "f5fafd",
   },
   {
     id: 2,
-    img: "https://o.remove.bg/downloads/47d15918-f0bd-4ae1-ac41-b67fcf25156d/taras-chernus--au3XMzfhro-unsplash-removebg-preview.png",
+    img: sliderImg2,
     title: "22 S/S 봄 컬렉션 한정 발매",
     desc: "Basic & Point가 돋보이는 22 S/S 봄 1차 컬렉션의 한정 발매 기획전입니다. 신상품을 단독 최대 56%의 가격으로 만나보세요.",
     bg: "fcf1ed",
   },
   {
     id: 3,
-    img: "https://o.remove.bg/downloads/2a77871c-c241-4fdc-a497-3f9756fe594c/photo-1622519407650-3df9883f76a5-removebg-preview.png",
+    img: sliderImg3,
     title: "신학기 아이템 모음전",
     desc: "트렌디하면서도 무드를 잃지 않은 디자인으로 만족할 수 있는 22 S/S 1차 컬렉션을 먼저 만나보세요.",
     bg: "fbf0f4",
@@ -37,5 +49,40 @@ export const categories = [
     id: 3,
     img: "https://images.unsplash.com/photo-1622434641406-a158123450f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHdhdGNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     title: "WATCH",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: productImg1,
+  },
+  {
+    id: 2,
+    img: productImg2,
+  },
+  {
+    id: 3,
+    img: productImg3,
+  },
+  {
+    id: 4,
+    img: productImg4,
+  },
+  {
+    id: 5,
+    img: productImg5,
+  },
+  {
+    id: 6,
+    img: productImg6,
+  },
+  {
+    id: 7,
+    img: productImg7,
+  },
+  {
+    id: 8,
+    img: productImg8,
   },
 ];
