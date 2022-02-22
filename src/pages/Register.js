1f1f24040df80bb2b8209ba91651cd4e5b25a581
amh6281 <amh6281@naver.com>
2022-02-22 14:28:07 +0900

    회원가입 및 로그인 페이지 생성 2022.02.22

---
+++ src/pages/Register.js
@@ -0,0 +1,78 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  padding: 20px;
+  width: 40%;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
+
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`;
+
+const Register = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>회원가입</Title>
+        <Form>
+          <Input placeholder="성" />
+          <Input placeholder="이름" />
+          <Input placeholder="아이디" />
+          <Input placeholder="이메일" />
+          <Input placeholder="비밀번호" />
+          <Input placeholder="비밀번호 확인" />
+          <Agreement>
+            <b>이용약관 및 개인정보수집 및 이용</b>에 모두 동의합니다.
+          </Agreement>
+          <Button>가입하기</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Register;
