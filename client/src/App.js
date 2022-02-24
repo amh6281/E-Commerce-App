c9ed808b61e1eec263af15950260327998c9ad3f
amh6281 <amh6281@naver.com>
2022-02-24 15:41:15 +0900

    react stripe 구현 2022.02.24
    StripeCheckout을 통해 결제 기능을 구현한다.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Pay from "./pages/Pay";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Pay />
+    </>
+  );
 }
 
 export default App;
