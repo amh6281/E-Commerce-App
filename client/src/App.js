a327661a01f371b04f8fbde0a105b5cafc2e28a7
amh6281 <amh6281@naver.com>
2022-02-25 14:22:14 +0900

    react-router 구현 2022.02.25
    각 페이지마다 경로를 지정하여 router를 구현하고, 카테고리 클릭 시 해당 카테고리로 이동할 수 있도록 Link를 지정한다.

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,24 @@ import Pay from "./pages/Pay";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-    <>
-      <Pay />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
