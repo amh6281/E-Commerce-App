ce282b5740ce65520cc0f1defe2aba9559ba0ed2
amh6281 <amh6281@naver.com>
2022-03-04 15:55:07 +0900

    Recharts 라이브러리를 활용한 Chart 컴포넌트 생성 2022.03.04
    Recharts 라이브러리를 사용하여 Chart 컴포넌트를 생성하고,
    title, data, dataKey, grid를 props로 하여 값을 지정한다.

--- admin/src/pages/home/Home.js
+++ admin/src/pages/home/Home.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
+import { userData } from "../../dummyData";
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        title="User Analytics"
+        data={userData}
+        grid
+        dataKey="Active User"
+      />
     </div>
   );
 };
