d69398e3a601f35cd2f56c90156d168f4ba7c455
amh6281 <amh6281@naver.com>
2022-03-11 13:40:50 +0900

    Node API에서 정보 가져오기 2022.03.11
    Node API에서 가입유저, 최근주문 내역을 가져온다.

--- admin/src/components/widgetLg/WidgetLg.js
+++ admin/src/components/widgetLg/WidgetLg.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { userRequest } from "../../requestMethods";
 import "./widgetLg.css";
+import { format } from "timeago.js";
 
 const WidgetLg = () => {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const getOrders = async () => {
+      try {
+        const res = await userRequest.get("orders");
+        setOrders(res.data);
+      } catch {}
+    };
+    getOrders();
+  }, []);
   const Button = ({ type }) => {
     return <button className={"widgetLgButton " + type}>{type}</button>;
   };
@@ -15,66 +28,18 @@ const WidgetLg = () => {
           <th className="widgetLgTh">Amount</th>
           <th className="widgetLgTh">Status</th>
         </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://user-images.githubusercontent.com/83646986/156501089-92b5bf79-321b-4868-a8e9-314fa164ff4e.png"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Amh</span>
-          </td>
-          <td className="widgetLgDate">2022-03-07</td>
-          <td className="widgetLgAmount">₩122,000</td>
-          <td className="widgetLgStatus">
-            <Button type="Approved" />
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://user-images.githubusercontent.com/83646986/156501089-92b5bf79-321b-4868-a8e9-314fa164ff4e.png"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Amh</span>
-          </td>
-          <td className="widgetLgDate">2022-03-07</td>
-          <td className="widgetLgAmount">₩122,000</td>
-          <td className="widgetLgStatus">
-            <Button type="Declined" />
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://user-images.githubusercontent.com/83646986/156501089-92b5bf79-321b-4868-a8e9-314fa164ff4e.png"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Amh</span>
-          </td>
-          <td className="widgetLgDate">2022-03-07</td>
-          <td className="widgetLgAmount">₩122,000</td>
-          <td className="widgetLgStatus">
-            <Button type="Pending" />
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://user-images.githubusercontent.com/83646986/156501089-92b5bf79-321b-4868-a8e9-314fa164ff4e.png"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Amh</span>
-          </td>
-          <td className="widgetLgDate">2022-03-07</td>
-          <td className="widgetLgAmount">₩122,000</td>
-          <td className="widgetLgStatus">
-            <Button type="Approved" />
-          </td>
-        </tr>
+        {orders.map((order) => (
+          <tr className="widgetLgTr" key={order._id}>
+            <td className="widgetLgUser">
+              <span className="widgetLgName">{order.userId}</span>
+            </td>
+            <td className="widgetLgDate">{format(order.createdAt)}</td>
+            <td className="widgetLgAmount">${order.amount}</td>
+            <td className="widgetLgStatus">
+              <Button type={order.status} />
+            </td>
+          </tr>
+        ))}
       </table>
     </div>
   );
--- admin/src/components/widgetSm/WidgetSm.js
+++ admin/src/components/widgetSm/WidgetSm.js
@@ -1,87 +1,46 @@
 import React from "react";
 import "./widgetSm.css";
 import { Visibility } from "@material-ui/icons";
+import { useState } from "react";
+import { useEffect } from "react";
+import { userRequest } from "../../requestMethods";
 
 const WidgetSm = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await userRequest.get("users/?new=true");
+        setUsers(res.data);
+      } catch {}
+    };
+    getUsers();
+  }, []);
+
   return (
     <div className="widgetSm">
       <span className="widgetSmTitle">New Join Members</span>
       <ul className="widgetSmList">
-        <li className="widgetSmListItem">
-          <img
-            src="https://user-images.githubusercontent.com/83646986/156501089-92b5bf79-321b-4868-a8e9-314fa164ff4e.png"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUserName">amh</span>
-            <span className="widgetSmUserTitle">hma</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://user-images.githubusercontent.com/83646986/156501089-92b5bf79-321b-4868-a8e9-314fa164ff4e.png"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUserName">amh</span>
-            <span className="widgetSmUserTitle">hma</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://user-images.githubusercontent.com/83646986/156501089-92b5bf79-321b-4868-a8e9-314fa164ff4e.png"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUserName">amh</span>
-            <span className="widgetSmUserTitle">hma</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://user-images.githubusercontent.com/83646986/156501089-92b5bf79-321b-4868-a8e9-314fa164ff4e.png"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUserName">amh</span>
-            <span className="widgetSmUserTitle">hma</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://user-images.githubusercontent.com/83646986/156501089-92b5bf79-321b-4868-a8e9-314fa164ff4e.png"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUserName">amh</span>
-            <span className="widgetSmUserTitle">hma</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
+        {users.map((user) => (
+          <li className="widgetSmListItem" key={users._id}>
+            <img
+              src={
+                users.img ||
+                "https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+              }
+              alt=""
+              className="widgetSmImg"
+            />
+            <div className="widgetSmUser">
+              <span className="widgetSmUserName">{user.username}</span>
+            </div>
+            <button className="widgetSmButton">
+              <Visibility />
+              Display
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
