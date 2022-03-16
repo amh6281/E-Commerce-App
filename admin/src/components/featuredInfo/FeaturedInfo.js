b61f5b38e0d26b9ed9d3c2120d239fed8952c356
amh6281 <amh6281@naver.com>
2022-03-16 13:08:51 +0900

    수익 계산 2022.03.16
    orders/income에서 데이터를 받아 수익을 계산한다.

--- admin/src/components/featuredInfo/FeaturedInfo.js
+++ admin/src/components/featuredInfo/FeaturedInfo.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./featuredInfo.css";
 import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+import { userRequest } from "../../requestMethods";
 
 const FeaturedInfo = () => {
+  const [income, setIncome] = useState([]);
+  const [perc, setPerc] = useState([]);
+
+  useEffect(() => {
+    const getIncome = async () => {
+      try {
+        const res = await userRequest.get("orders/income");
+        setIncome(res.data);
+        setPerc((res.data[1].total * 100) / res.data[0].total - 100);
+      } catch {}
+    };
+    getIncome();
+  }, []);
+
   return (
     <div className="featured">
       <div className="featuredItem">
         <span className="featuredTitle">수익</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">₩2,415,000</span>
+          <span className="featuredMoney">₩{income[1].total}</span>
           <span className="featuredMoneyRate">
-            -11.4 <ArrowDownward className="featuredIcon negative" />
+            {Math.floor(perc)}%{" "}
+            {perc < 0 ? (
+              <ArrowDownward className="featuredIcon negative" />
+            ) : (
+              <ArrowUpward className="featuredIcon" />
+            )}
           </span>
         </div>
         <span className="featuredSub">전월 대비</span>
